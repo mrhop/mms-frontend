46d3084e540ace239dadcbccb732190fa09854e5
mrhop <233402923@qq.com>
2018-03-15 14:46:32 +0800

    access control module - authority

--- src/components/index/accessControl/authority/Add.jsx
+++ src/components/index/accessControl/authority/Add.jsx
@@ -5,13 +5,12 @@ import React, {Component, Fragment} from 'react';
 import {connect} from 'react-redux'
 import PropTypes from 'prop-types'
 
-import {Form, Icon, Input, Button, Row, Col, Alert, message, Spin} from 'antd';
+import {Form, Icon, Input, Button, TreeSelect, Col, Alert, Select, Checkbox, Spin} from 'antd';
 
 import {history} from '../../../../redux/index/store'
-import {accessControlActions} from '../../../../redux/index/actions'
+import {accessControlActions, optionActions} from '../../../../redux/index/actions'
 import * as ActionTypes from '../../../../redux/index/actions/ActionTypes'
 import {formItemLayout, formItemTailLayout} from '../../../../common/FormLayout';
-
 
 class Add extends Component {
   constructor(props) {
@@ -19,15 +18,21 @@ class Add extends Component {
     this.state = {
       loading: false
     }
-    this.props.getPostSingle()
+    this.props.getAuthoritySingle()
+    this.props.getPostOptions()
+    this.props.getAuthorityParentTreeOptions()
   }
 
   componentWillReceiveProps(nextProps) {
-    const {type} = nextProps
-    if (type === ActionTypes.ACCESSCONTROL_POST_SAVE_SUCCESS) {
-      history.go(-1)
-    } else if (type === ActionTypes.ACCESSCONTROL_POST_SAVE_FAILURE) {
+    const {type, postOptions, authorityParentOptions} = nextProps
+    if (type === ActionTypes.ACCESSCONTROL_AUTHORITY_SAVE_SUCCESS) {
+      history.push('/accesscontrol/authority')
+    } else if (type === ActionTypes.ACCESSCONTROL_AUTHORITY_SAVE_FAILURE) {
       this.setState({loading: false})
+    } else if (postOptions && postOptions.type === ActionTypes.ACCESSCONTROL_POST_OPTIONS_GOT) {
+      this.setState({postOptions: postOptions.data})
+    } else if (authorityParentOptions && authorityParentOptions.type === ActionTypes.ACCESSCONTROL_AUTHORITYPARENT_TREE_OPTIONS_GOT) {
+      this.setState({authorityParentOptions: authorityParentOptions.data})
     }
   }
 
@@ -40,19 +45,22 @@ class Add extends Component {
     this.props.form.validateFields((err, values) => {
       if (!err) {
         this.setState({loading: true})
-        this.props.savePost(values)
+        this.props.saveAuthority(values)
       }
     });
   }
 
   render() {
-    const {loading} = this.state
+    const {loading, postOptions, authorityParentOptions} = this.state
+    const {Option} = Select;
+    let postOptionsArr = postOptions && postOptions.map(function (item) {
+        return <Option key={item.value} value={item.value}>{item.text}</Option>
+      })
     const {type} = this.props
     const FormItem = Form.Item
-    const {TextArea} = Input;
     const {getFieldDecorator} = this.props.form
     let alertMsg = null
-    if (type === ActionTypes.ACCESSCONTROL_POST_SAVE_FAILURE) {
+    if (type === ActionTypes.ACCESSCONTROL_AUTHORITY_SAVE_FAILURE) {
       alertMsg = <Alert
         message="保存失败"
         description="失败原因，后台传回"
@@ -64,18 +72,47 @@ class Add extends Component {
                  spinning={loading}>
       {alertMsg}
       <Form onSubmit={this.submitFun}>
-        <FormItem label="职位名称" {...formItemLayout}>
-          {getFieldDecorator('name', {
+        <FormItem label="名称" {...formItemLayout}>
+          {getFieldDecorator('name',
+            {
+              rules: [{
+                required: true, message: '必选字段!'
+              }],
+            })(
+            <Input placeholder="名称"/>
+          )}
+        </FormItem>
+        <FormItem label="职位" {...formItemLayout}>
+          {getFieldDecorator('post', {
             rules: [{
               required: true, message: '必选字段!'
             }],
           })(
-            <Input placeholder="职位名称"/>
+            <Select placeholder="职位">
+              {postOptionsArr || []}
+            </Select>
           )}
         </FormItem>
-        <FormItem label="详细说明" {...formItemLayout}>
-          {getFieldDecorator('description')(
-            <TextArea placeholder="详细说明"/>
+        <FormItem label="导航显示" {...formItemLayout}>
+          {getFieldDecorator('inMenu', {
+            initialValue: true
+          })(
+            <Checkbox>是</Checkbox>
+          )}
+        </FormItem>
+        <FormItem label="父权限" {...formItemLayout}>
+          {getFieldDecorator('parent')(
+            <TreeSelect
+              dropdownStyle={{ maxHeight: 400, overflow: 'auto' }}
+              treeData={authorityParentOptions}
+              placeholder="父权限"
+              allowClear
+            />
+          )}
+        </FormItem>
+        <FormItem label="URL" {...formItemLayout}>
+          {getFieldDecorator('url')(
+            <Input placeholder="URL"/>
           )}
         </FormItem>
         <FormItem {...formItemTailLayout}>
@@ -91,18 +128,26 @@ class Add extends Component {
 Add.propTypes = {
   location: PropTypes.object,
   type: PropTypes.string,
-  savePost: PropTypes.func,
-  getPostSingle: PropTypes.func,
+  saveAuthority: PropTypes.func,
+  getAuthoritySingle: PropTypes.func,
+  getPostOptions: PropTypes.func,
+  getAuthorityParentTreeOptions: PropTypes.func,
   form: PropTypes.object.isRequired,
+  postOptions: PropTypes.object,
+  authorityParentOptions: PropTypes.object
 }
 
 
 const mapStateToProps = (state) => {
-  return {...state.accessControlPostSingle}
+  return {
+    ...state.accessControlAuthoritySingle,
+    postOptions: state.accessControlPostOptions,
+    authorityParentOptions: state.accessControlAuthorityParentTreeOptions
+  }
 }
 
 const mapDispatchToProps = {
-  ...accessControlActions
+  ...accessControlActions, ...optionActions
 }
 const AddProxy = connect(
   mapStateToProps,
